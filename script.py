5d44d5be70630baad9f9a28ba99559ef93927e46
Sarthak <sainisarthak07@gmail.com>
2021-03-05 22:04:30 +0530

    Initial Commit

---
+++ script.py
@@ -0,0 +1,19 @@
+import sys
+import requests
+
+print(sys.version)
+print(sys.executable)
+
+
+def greet(who_to_greet):
+    greeting = 'Hello, this is a script to get the {}'.format(who_to_greet)
+    return greeting
+
+
+print(greet('Gyantech Website Status Code'))
+
+r = requests.get('http://gyantech.in')
+print(r.status_code)
+
+if r.status_code == 200:
+    print('Sarthak Saini')
---
+++ script2.py
@@ -0,0 +1,5 @@
+import sys
+import requests
+
+name = input("Your name? ")
+print("Hello, ", name)
